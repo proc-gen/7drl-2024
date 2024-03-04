5efbb9a9a05ecde803caeba197225cd56776ee12
Micah Caunter <mcaunter12@gmail.com>
2024-03-03 21:01:05 -0500

    feat: add map generators sans spawners

---
+++ Magi/Magi/Maps/Generators/RoomsAndCorridorsGenerator.cs
@@ -0,0 +1,128 @@
+﻿using Magi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magi.Maps.Generators
+{
+    public class RoomsAndCorridorsGenerator : Generator
+    {
+        static Tile Wall = new Tile()
+        {
+            BaseTileType = Constants.TileTypes.Wall,
+            BackgroundColor = new Color(0f, 0f, .5f)
+        };
+        static Tile FloorA = new Tile()
+        {
+            BaseTileType = Constants.TileTypes.Floor,
+            BackgroundColor = Color.LightGray
+        };
+        static Tile FloorB = new Tile()
+        {
+            BaseTileType = Constants.TileTypes.Floor,
+            BackgroundColor = Color.WhiteSmoke
+        };
+
+        List<Rectangle> Rooms { get; set; }
+
+        public RoomsAndCorridorsGenerator(int width, int height)
+            : base(width, height)
+        {
+            Rooms = new List<Rectangle>();
+        }
+
+        public override void Generate()
+        {
+            for (int i = 0; i < Map.Width; i++)
+            {
+                for (int j = 0; j < Map.Height; j++)
+                {
+                    Map.SetTile(i, j, Wall);
+                }
+            }
+
+            int maxRooms = 30, minSize = 6, maxSize = 10;
+
+            Random random = new Random();
+
+            for (int i = 0; i < maxRooms; i++)
+            {
+                int roomWidth = random.Next(minSize, maxSize);
+                int roomHeight = random.Next(minSize, maxSize);
+                int x = random.Next(1, Map.Width - roomWidth - 1) - 1;
+                int y = random.Next(1, Map.Height - roomHeight - 1) - 1;
+
+                Rectangle room = new Rectangle(x, y, roomWidth, roomHeight);
+                bool canAdd = true;
+                if (Rooms.Any() && Rooms.Exists(a => a.Intersects(room)))
+                {
+                    canAdd = false;
+                }
+                if (canAdd)
+                {
+                    ApplyRoomToMap(room);
+                    if (Rooms.Any())
+                    {
+                        Point newCenter = room.Center;
+                        Point oldCenter = Rooms.Last().Center;
+
+                        if (random.Next(0, 2) == 1)
+                        {
+                            ApplyHorizontalTunnel(oldCenter.X, newCenter.X, oldCenter.Y);
+                            ApplyVerticalTunnel(oldCenter.Y, newCenter.Y, newCenter.X);
+                        }
+                        else
+                        {
+                            ApplyVerticalTunnel(oldCenter.Y, newCenter.Y, oldCenter.X);
+                            ApplyHorizontalTunnel(oldCenter.X, newCenter.X, newCenter.Y);
+                        }
+                    }
+                    Rooms.Add(room);
+                }
+            }
+        }
+        public override Point GetPlayerStartingPosition()
+        {
+            return Rooms.First().Center;
+        }
+
+        public override void SpawnEntitiesForMap(GameWorld world)
+        {
+            
+        }
+
+        public override void SpawnExitForMap(GameWorld world)
+        {
+            
+        }
+
+        private void ApplyRoomToMap(Rectangle room)
+        {
+            for (int i = room.X + 1; i <= room.MaxExtentX; i++)
+            {
+                for (int j = room.Y + 1; j <= room.MaxExtentY; j++)
+                {
+                    Map.SetTile(i, j, i % 2 == j % 2 ? FloorA : FloorB);
+                }
+            }
+        }
+
+        private void ApplyHorizontalTunnel(int x1, int x2, int y)
+        {
+            for (int i = Math.Min(x1, x2); i <= Math.Max(x1, x2); i++)
+            {
+                Map.SetTile(i, y, i % 2 == y % 2 ? FloorA : FloorB);
+            }
+        }
+
+        private void ApplyVerticalTunnel(int y1, int y2, int x)
+        {
+            for (int j = Math.Min(y1, y2); j <= Math.Max(y1, y2); j++)
+            {
+                Map.SetTile(x, j, x % 2 == j % 2 ? FloorA : FloorB);
+            }
+        }
+    }
+}
---
+++ Magi/Magi/Maps/Map.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magi.Maps
+{
+    public class Map
+    {
+        [JsonProperty]
+        protected Tile[] MapGrid { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            MapGrid = new Tile[Width * Height];
+        }
+
+        public Tile GetTile(int x, int y)
+        {
+            return MapGrid[y * Width + x];
+        }
+
+        public Tile GetTile(Point point)
+        {
+            return GetTile(point.X, point.Y);
+        }
+
+        public void SetTile(int x, int y, Tile tile)
+        {
+            MapGrid[y * Width + x] = tile;
+        }
+
+        public void SetTile(Point point, Tile tile)
+        {
+            SetTile(point.X, point.Y, tile);
+        }
+    }
+}
