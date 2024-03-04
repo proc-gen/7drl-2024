db532952ab58c6a76976812303c44410f1f9afe9
Micah Caunter <65076703+proc-gen@users.noreply.github.com>
2024-03-03 20:35:18 -0500

    Add screens (#3)
    * feat: make RootScreen into a screen manager and add MainMenuScreen
    
    * feat: add console draw extensions for boxes
    
    * feat: some ui changes to the main menu
    
    * feat: add game screen
    
    * feat: add simple splash screen

---
+++ Magi/Magi/Constants/Screens.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magi.Constants
+{
+    public enum Screens
+    {
+        Splash,
+        MainMenu,
+        Game,
+        GameOver
+    }
+}
---
+++ Magi/Magi/UI/MainMenuScreen.cs
@@ -0,0 +1,129 @@
+﻿using Magi.Constants;
+using Magi.Scenes;
+using Magi.UI.Helpers;
+using SadConsole.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magi.UI
+{
+    public class MainMenuScreen : ScreenObject
+    {
+        RootScreen RootScreen;
+
+        ScreenSurface screen;
+        bool dirty = true;
+
+        int selectedButtonIndex = 0;
+
+        public MainMenuScreen(RootScreen rootScreen)
+        {
+            RootScreen = rootScreen;
+            screen = new ScreenSurface(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT);
+        }
+
+        public override void Update(TimeSpan delta)
+        {
+            HandleKeyboard();
+            RefreshScreen();
+            base.Update(delta);
+        }
+
+        private void HandleKeyboard()
+        {
+            var keyboard = Game.Instance.Keyboard;
+
+            if (keyboard.IsKeyPressed(Keys.Up) && selectedButtonIndex > 0)
+            {
+                if (showContinue())
+                {
+                    selectedButtonIndex--;
+                }
+                else
+                {
+                    selectedButtonIndex = 0;
+                }
+                dirty = true;
+            }
+            else if (keyboard.IsKeyPressed(Keys.Down) && selectedButtonIndex < 2)
+            {
+                if (showContinue())
+                {
+                    selectedButtonIndex++;
+                }
+                else
+                {
+                    selectedButtonIndex = 2;
+                }
+                dirty = true;
+            }
+            else if (keyboard.IsKeyDown(Keys.Enter))
+            {
+                switch (selectedButtonIndex)
+                {
+                    case 0:
+                        RootScreen.AddScreen(Screens.Game, new GameScreen(RootScreen, false));
+                        break;
+                    case 1:
+                        RootScreen.AddScreen(Screens.Game, new GameScreen(RootScreen, true));
+                        break;
+                    case 2:
+                        Game.Instance.MonoGameInstance.Exit();
+                        break;
+                }
+            }
+        }
+
+        private void RefreshScreen()
+        {
+            if (dirty)
+            {
+                screen.Clear();
+                printTitle();
+
+                int optionPosition = 24;
+
+                printMenuOption(screen.Width / 2 - 4, optionPosition, "New Game", selectedButtonIndex == 0);
+                optionPosition++;
+
+                if (showContinue())
+                {
+                    printMenuOption(screen.Width / 2 - 6, optionPosition, "Continue Game", selectedButtonIndex == 1);
+                    optionPosition++;
+                }
+
+                printMenuOption(screen.Width / 2 - 4, optionPosition, "Quit Game", selectedButtonIndex == 2);
+
+                dirty = false;
+            }
+        }
+
+        private void printTitle()
+        {
+            screen.DrawRLTKStyleBox(screen.Width / 2 - 15, 13, 30, 4, Color.White, Color.Black);
+            screen.Print(screen.Width / 2 - 11, 15, "Land of the Fallen Magi", Color.Yellow, Color.Black);
+        }
+
+        private void printMenuOption(int x, int y, string text, bool active)
+        {
+            screen.Print(x, y, text, Color.White, Color.Black);
+            if (active)
+            {
+                screen.Print(x - 3, y, "->", Color.White, Color.Black);
+            }
+        }
+
+        private bool showContinue()
+        {
+            return File.Exists("savegame.json");
+        }
+
+        public override void Render(TimeSpan delta)
+        {
+            screen.Render(delta);
+        }
+    }
+}
