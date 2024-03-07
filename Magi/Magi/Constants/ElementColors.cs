d141e39459a776ce1323c0606043a657d351cc61
Micah Caunter <mcaunter12@gmail.com>
2024-03-06 21:30:08 -0500

    feat: add element colors for constants

---
+++ Magi/Magi/Constants/ElementColors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magi.Constants
+{
+    public static class ElementColors
+    {
+        public static readonly Color FireOrange = new Color(0xef8c10ff);
+        public static readonly Color FireYellow = new Color(0xe2ef10ff);
+        public static readonly Color FireRed = new Color(0xef1d10ff);
+        public static readonly Color IceCyan = new Color(0x29d6d5ff);
+        public static readonly Color IceBlue = new Color(0x2980d6ff);
+        public static readonly Color IceBlueGreen = new Color(0x29d67fff);
+        public static readonly Color LightningVeryLightYellow = new Color(0xf7ffc4ff);
+        public static readonly Color LightningLightYellow = new Color(0xf0ff91ff);
+        public static readonly Color LightningWhite = new Color(0xffffffff);
+        public static readonly Color EarthDarkBrown = new Color(0x9c4f32ff);
+        public static readonly Color EarthBrown = new Color(0xc26340ff);
+        public static readonly Color EarthLightBrown = new Color(0xce8264ff);
+        public static readonly Color WaterFadedBlue = new Color(0x0060ffff);
+        public static readonly Color WaterMediumBlue = new Color(0x2000ffff);
+        public static readonly Color WaterIceBlue = new Color(0x00dfffff);
+        public static readonly Color AirVeryLightGrey = new Color(0xe1e2e2ff);
+        public static readonly Color AirLightGrey = new Color(0xc7cac9ff);
+        public static readonly Color AirWhite = new Color(0xffffffff);
+    }
+}
